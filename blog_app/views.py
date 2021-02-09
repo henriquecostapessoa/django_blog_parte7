111b87f8feea800981466f90a59d2e5166165a8a
Henrique Costa Pessoa <hcostapessoa@gmail.com>
2021-02-09 15:03:01 +0000

    first commit

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-       'autor': 'Jorge Jesus',
-       'titulo': 'post 1',
-       'conteudo': 'primeiro post com conteudo',
-       'data_postada': '28 de Janeiro, 2021'
-    },
-    {
-       'autor': 'Abel Ferreira',
-       'titulo': 'post 2',
-       'conteudo': 'segundo post com conteudo',
-       'data_postada': '02 de Fevereiro, 2021'
-    },
-    
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog_app/home.html', context)
 
